44720c8b386b887c6dc7a3a64e4b9180e03a1799
sofy sofya <sofy7468612@gmail.com>
2022-11-24 20:21:56 +0530

    firstagain

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const { connect } = require("mongoose")
 
 const connectDb = async () => {
- return connect(process.env.DB_URI)
+ return connect(process.env.DB_URI )
 }
 
 module.exports = { connectDb }
