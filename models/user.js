3386d7cd455e20a311ab7360d66c6ce89141bfc0
sofy sofya <sofy7468612@gmail.com>
2022-11-25 17:49:18 +0530

    changes

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const UserModel = model(
   new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, required: true,default:"user" },
+    role: { type: String, required: true },
   })
 )
 
